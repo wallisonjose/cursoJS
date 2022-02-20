85f05cdbfc485be4c71d042da54576629ef55848
Wallison José <wallisonjose22@outlook.com>
2022-02-20 16:19:12 -0300

    Update operacao-ternaria.js

--- Lógica de Programação/operacao-ternaria.js
+++ Lógica de Programação/operacao-ternaria.js
@@ -1,2 +1,3 @@
 let chuva = true
 let oQueLevar = () => chuva ? 'Levar capa de chuva' : 'Levar boné'
+console.log(oQueLevar()) // O retorno será 'levar capa de chuva', visto que a variável chuva é true
