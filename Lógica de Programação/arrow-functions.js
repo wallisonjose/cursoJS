39a1036d4c625da677cffc412bf3f9242803f24f
Wallison José <wallisonjose22@outlook.com>
2022-02-20 16:16:13 -0300

    Create arrow-functions.js

---
+++ Lógica de Programação/arrow-functions.js
@@ -0,0 +1,15 @@
+// Caso a lógica seja curta não há a necessidade de abrir e fechar chaves
+
+let funcao = (x, y) => return x + y
+
+funcao(2,3)
+
+ or 
+
+// Caso a lógica seja longa há a possibilidade de abrir e fechar chaves
+
+let funcao2 = () => {
+	return 'Hello World'
+}
+
+console.log(funcao2())
